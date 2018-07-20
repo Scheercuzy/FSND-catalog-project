efd5e986f00c0adbc5e2b21c8db040006971d91f
Scheercuzy <maxi730@gmail.com>
2018-07-20 14:59:49 +0630

    Created Database Models

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from catalog.routes import url
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.register_blueprint(url)
+app.config['SECRET_KEY'] = '6eL0O65HUS3RMcAQTFuz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
