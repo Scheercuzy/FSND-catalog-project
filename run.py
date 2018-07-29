2700929885288d58b6439411b75c4940de99b4c6
Scheercuzy <maxi730@gmail.com>
2018-07-29 20:25:51 +0630

    Fixed run.py

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import sys
 from catalog import app, db
 
+
+def setup():
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+        print("Database tables created")
+
+
 if __name__ == '__main__':
     if "--setup" in sys.argv:
-        with app.app_context():
-            db.create_all()
-            db.session.commit()
-            print("Database tables created")
+        setup()
     else:
         app.debug = True
         app.run(host='0.0.0.0', port=5000)
